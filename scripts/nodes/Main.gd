f90f0960f2787a77ca886f6ab51334160748dd84
harold-kalvin <harold.k.basa@gmail.com>
2020-06-24 23:15:54 +0200

    make NumButton clickable

--- scripts/nodes/Main.gd
+++ scripts/nodes/Main.gd
@@ -7,4 +7,9 @@ func _ready():
     var button = num_button.instance()
     button.position = Vector2(300, 300)
     button.init("1")
+    button.connect("pressed", self, "_on_num_button_pressed")
     add_child(button)
+
+
+func _on_num_button_pressed(button):
+    print("pressed ", button)
--- scripts/nodes/NumButton.gd
+++ scripts/nodes/NumButton.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+signal pressed
+
 var num_str = ""
 
 
 func _ready():
+    $ClickArea.connect("input_event", self, "_on_click_area_input_event")
+
     set_num()
     set_num_position()
 
@@ -20,3 +24,9 @@ func set_num_position():
     var size = $Num.get_combined_minimum_size()
     var pos = $Num.rect_position
     $Num.rect_position = Vector2(pos.x - size.x / 2, pos.y - size.y / 2)
+
+
+func _on_click_area_input_event(_viewport, event, _shape_idx):
+    if event is InputEventScreenTouch:
+        if event.pressed:
+            emit_signal("pressed", self)
