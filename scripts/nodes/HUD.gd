508562c45a234b3fd15b9ac49f3299092fff08bf
harold-kalvin <harold.k.basa@gmail.com>
2020-08-16 20:36:17 +0200

    change color of dialed number and change objective to RichTextLabel

--- scripts/nodes/HUD.gd
+++ scripts/nodes/HUD.gd
@@ -9,6 +9,8 @@ onready var level = $VBoxContainer/HBoxContainer/LevelContainer/Level
 onready var objective = $VBoxContainer/HBoxContainer/ObjectiveContainer/Objective
 onready var progress_bar = $VBoxContainer/TimerContainer/TimerProgressBar
 
+const DIALED_COLOR = Color("#21183a")
+
 
 func _ready():
     # scale and position HUD with main block (aspect ratio)
@@ -25,10 +27,19 @@ func _ready():
     game.connect("max_score_changed", self, "_on_max_score_changed")
     game.connect("level_changed", self, "_on_level_changed")
     game.connect("objective_changed", self, "_on_objective_changed")
+    game.connect("dial_succeded", self, "_on_dial_succeded")
+
+    # align objective (RichTextLabel) 
+    yield(get_tree(), "idle_frame")
+    _align_objective()
 
 
 func _process(_delta):
     progress_bar.value = timer.time_left
+
+
+func _align_objective():
+    objective.rect_min_size = objective.get_font("normal_font").get_string_size(objective.text)
 
 
 func _on_score_changed(new_score):
@@ -45,3 +56,19 @@ func _on_level_changed(new_level):
 
 func _on_objective_changed(new_objective):
     objective.text = str(new_objective)
+    _align_objective()
+
+
+func _on_dial_succeded():
+    objective.clear()
+
+    # change color of dialed numbers
+    objective.push_color(DIALED_COLOR)
+    objective.add_text(game.get_dialed())
+    objective.pop()
+
+    # keep undialed numbers unchanged
+    var to_be_dialed = game.get_objective().right(game.get_dialed().length())
+    objective.add_text(to_be_dialed)
+
+    _align_objective()
